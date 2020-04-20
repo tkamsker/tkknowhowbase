bba96b98ce70d03447e9cd62e439b39c2d564b6f
Thomas Kamsker <tkamsker@tkamsker.at>
2020-04-20 16:15:36 +0200

    erste Version suche nicht ok

--- src/components/ResultCards.js
+++ src/components/ResultCards.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import Card, { CardActions, CardContent, CardMedia } from '@material-ui/core';
+// import Card, { CardActions, CardContent, CardMedia } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 // import Button from 'material-ui/Button'
 // import Typography from 'material-ui/Typography'
 // import Grid from 'material-ui/Grid'
